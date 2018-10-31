4005c0bd0d509f3410635dc8a52d9bf197a1a590
Lars <larsmackenzie@gmail.com>
2018-10-31 18:18:18 -0500

    created sessions table

--- src/components/SessionsTable/SessionsTable.js
+++ src/components/SessionsTable/SessionsTable.js
@@ -1,10 +1,56 @@
 import React, {Component} from 'react'; 
+import { connect } from 'react-redux'; 
+import { Table, TableCell, TableRow, TableHead, TableBody, Button } from '@material-ui/core'; 
 
 class SessionsTable extends Component {
     render(){
         return(
-            <div>Sessions Table</div>
+            <div>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell>One</TableCell>
+                            <TableCell>Two</TableCell>
+                            <TableCell>Three</TableCell>
+                            <TableCell>Four</TableCell>
+                            <TableCell>Five</TableCell>
+                            <TableCell>Six</TableCell>
+                            <TableCell>Seven</TableCell>
+                            <TableCell>Eight</TableCell>
+                            <TableCell>Nine</TableCell>
+                            <TableCell>Ten</TableCell>
+                            <TableCell>Contact</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.props.sessions.map((session, i) => {
+                            return (
+                                <TableRow key={i}>
+                                    <TableCell>{session.first_name} {session.last_name}</TableCell>
+                                    <TableCell>{session.date}</TableCell>
+                                    <TableCell>{session.date}</TableCell>
+                                    <TableCell>{session.date}</TableCell>
+                                    <TableCell>{session.date}</TableCell>
+                                    <TableCell>{session.date}</TableCell>
+                                    <TableCell>{session.date}</TableCell>
+                                    <TableCell>{session.date}</TableCell>
+                                    <TableCell>{session.date}</TableCell>
+                                    <TableCell>{session.date}</TableCell>
+                                    <TableCell>{session.date}</TableCell>
+                                    <TableCell><Button>Email</Button></TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+
+            </div>
         );
     }
 }
-export default SessionsTable; 
+
+const mapStateToProps = state => ({
+    sessions: state.sessions.sessions,
+  });
+export default connect(mapStateToProps)(SessionsTable); 
--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,72 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button, Dialog, DialogTitle, DialogContent, DialogActions, NativeSelect, TextField } from '@material-ui/core';
-import moment from 'moment'; 
+import SessionsTable from '../SessionsTable/SessionsTable';
+import LogSessions from '../LogSessionForm/LogSessionForm'; 
 
 class UserPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       open: false,
-      client: '',
-      date: ''
     }
   }
   componentDidMount(){
     this.getClients();
-    this.getDate();
+    this.getSessions();
   }
   getClients = () => {
     this.props.dispatch({type: 'FETCH_CLIENTS'});
   }
-  getDate = () => {
-    let date = new Date(); 
-    let today = moment(date).format('YYYY-MM-DD');
-    this.setState({
-      date: today
-    });
-    console.log(today); 
+  getSessions = () => {
+    this.props.dispatch({type: 'FETCH_SESSIONS'}); 
   }
-  handleChangeFor = (event, property) => {
-    this.setState({
-      [property]: event.target.value 
-    });
-  }
-  handleClose = () => {
-    this.setState({
-      open: false
-    })
-  }
-  handleOpen = () => {
-    this.setState({
-      open: true
-    })
-  }
-  logSession = () => {
-    this.handleClose(); 
-    this.props.dispatch({type: 'LOG_SESSION', payload: {client: this.state.client, date: this.state.date}})
-  }
+
   render() {
     return (
-      <div className="flex-container-home">
-        <Button onClick={this.handleOpen} color="primary">Log Sessions</Button>
-        <Dialog open={this.state.open} onClose={this.handleClose}>
-            <DialogTitle>Log Session</DialogTitle>
-            <DialogContent>
-            <NativeSelect onChange={(event)=>this.handleChangeFor(event, 'client')} value={this.state.client}>
-              {this.props.clients.map((client, i) => {
-                return (
-                  <option key={i} value={client.id}>{client.first_name} {client.last_name}</option>
-                )
-              })}
-              </NativeSelect>
-              <TextField onChange={(event)=>this.handleChangeFor(event, 'date')} type="date" value={this.state.date} />
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={this.handleClose}>Cancel</Button>
-              <Button onClick={this.logSession}>Submit</Button>
-            </DialogActions>
-        </Dialog>
+      <div>
+        <LogSessions/>
+        <SessionsTable/>
       </div>
     );
   }
@@ -74,7 +33,6 @@ class UserPage extends Component {
 
 const mapStateToProps = state => ({
   user: state.user,
-  clients: state.clients.clients
 });
 
 export default connect(mapStateToProps)(UserPage);
