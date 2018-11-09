99f640a5dadcad846ee9e0a68c27dccbad2089b9
Lars <larsmackenzie@gmail.com>
2018-11-09 12:45:19 -0600

    removed register route

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -16,15 +16,15 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 });
 
 // Handles POST request with new user data
-router.post('/register', (req, res, next) => {  
-  const username = req.body.username;
-  const password = encryptLib.encryptPassword(req.body.password);
+// router.post('/register', (req, res, next) => {  
+//   const username = req.body.username;
+//   const password = encryptLib.encryptPassword(req.body.password);
 
-  const queryText = 'INSERT INTO person (username, password) VALUES ($1, $2) RETURNING id';
-  pool.query(queryText, [username, password])
-    .then(() => { res.sendStatus(201); })
-    .catch((err) => { next(err); });
-});
+//   const queryText = 'INSERT INTO person (username, password) VALUES ($1, $2) RETURNING id';
+//   pool.query(queryText, [username, password])
+//     .then(() => { res.sendStatus(201); })
+//     .catch((err) => { next(err); });
+// });
 
 // Handles login form authenticate/login POST
 // userStrategy.authenticate('local') is middleware that we run on this route
