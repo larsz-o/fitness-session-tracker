18833e469af77c40889119303f2a90bee096a10f
Lars Mackenzie <larsmackenzie@gmail.com>
2019-08-04 13:35:47 -0400

    making some styling changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Footer.css'
 
-const Footer = () => (
-  <footer>
-    <p className="center-footer">Fitness Meets Wellness &copy; 2018 </p>
-  </footer>
-);
+class Footer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      year: ''
+    }
+  }
+  componentDidMount(){
+    let date = new Date();
+    let year = date.getFullYear(); 
+    this.setState({
+      year: year
+    })
+  }
+  render(){
+    return( <footer>
+      <p className="center-footer">Fitness Meets Wellness &copy; {this.state.year} </p>
+    </footer>);
+  }
+} 
 
 export default Footer;
